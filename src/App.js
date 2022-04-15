9b0fdc74c648fc17fba2e4258c18ec7df26b4c09
Ayon <ayonjodder177@gmail.com>
2022-04-15 22:04:19 +0600

    Banner done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navber from './Components/Shared/Navber';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
-
+      <Navber></Navber>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
